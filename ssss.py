6f24dad4dc16964a9f89135b32522eb4055940c6
Kanna99 <30925616+Kanna99@users.noreply.github.com>
2018-07-12 15:07:23 +0530

    Update ssss.py

--- ssss.py
+++ ssss.py
@@ -1,11 +1,10 @@
-a=int(input())
-b=int(input())
-c=int(input())
-if (a>b and a>c):
-    print("a is greater")
-elif (b>c and b>a):
-    print("b is not greater")
-elif (a==b and b==c):
-    print("a,b,c is same")
-else:
-    print("c is greater")
+a,b,c=input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+if (a>=b and a>=c):
+    print(a)
+elif (b>=c and b>=a):
+    print(b)
+elif(c>=a and c>=b):
+    print(c)
