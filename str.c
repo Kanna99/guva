09968da1426a6ccabf728b6bd38344922db9241a
Kanna99 <30925616+Kanna99@users.noreply.github.com>
2018-07-13 14:33:30 +0530

    Create str.c

---
+++ str.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void append(char s[])
+{
+  int x=strlen(s);
+  s[x]='.';
+  s[x+1]='\0';
+  printf("%s",s);
+}
+int main()
+{
+      char s[20];
+      scanf("%s",s);
+      append(s);
+      return 0;
+}
